2166d74109ea717ed2a058b6895d3b81778ed280
alvinjizdan23 <vinzz.j01@gmail.com>
2022-11-16 12:44:11 +0700

    update

---
+++ views/products.php
@@ -0,0 +1,64 @@
+<div class="container container-fluid p-5">
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Produk</th>
+      <th scope="col">Tipe Produk</th>
+      <th scope="col">Harga</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Paket PlayStation 5 Include Disk Gaming Fullset</td>
+      <td>Produk Console</td>
+      <td>Rp 15.500.000</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>PlayStation 4 PRO (Wireless) + Hardisk + Games</td>
+      <td>Produk Console</td>
+      <td>Rp 5.800.000</td>
+      </tr>
+      </tr>
+      <tr>
+      <th scope="row">3</th>
+      <td>XBOX Series S Microsoft Digital Edition</td>
+      <td>Produk Console</td>
+      <td>Rp 3.880.000</td>
+    </tr>
+      <tr>
+      <th scope="row">4</th>
+      <td>PS5 God Of War Ragnarok R3</td>
+      <td>CD Playstation</td>
+      <td>Rp 1.029.000</td>
+    </tr>
+    </tr>
+      <tr>
+      <th scope="row">5</th>
+      <td>PS5 Spiderman Miles Morales - R2</td>
+      <td>CD Playstation</td>
+      <td>Rp 499.000</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Action Figure GOW</td>
+      <td>Action Figure</td>
+      <td>Rp 450.000</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Rexus Gladius GX100 - Wireless Gaming Controller PC/PS/XBOX </td>
+      <td>Controller</td>
+      <td>Rp 216.000</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Flashdisk SanDisk Cruzer Blade 64GB ISI GAME PC</td>
+      <td>Bundle Game</td>
+      <td>Rp 165.000</td>
+    </tr>
+  </tbody>
+</table>
+</div>
